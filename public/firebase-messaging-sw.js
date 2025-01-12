64509f29eacb82617b11cca0fd75cd2a2bcdd38c
semikas <104554495+semikas@users.noreply.github.com>
2025-01-12 10:12:08 +0530

    Remove firebase config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,13 +7,13 @@ importScripts(
 // Initialize the Firebase app in the service worker
 // "Default" Firebase configuration (prevents errors)
 const firebaseConfig = {
-    apiKey: "AIzaSyCDwYQtMp2dFZ9aaGy5TcA8s5ujFGP6zZM",
-    authDomain: "my-first-project-6c51a.firebaseapp.com",
-    projectId: "my-first-project-6c51a",
-    storageBucket: "my-first-project-6c51a.firebasestorage.app",
-    messagingSenderId: "233816787125",
-    appId: "1:233816787125:web:b7c15bcd4cb1a5b02956fc",
-    measurementId: "G-H16FB6Y2TC"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 firebase.initializeApp(firebaseConfig);
--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAnalytics, logEvent } from "firebase/analytics";
 
 //Firebase Config values imported from .env file
 const firebaseConfig = {
-  apiKey: "AIzaSyCDwYQtMp2dFZ9aaGy5TcA8s5ujFGP6zZM",
-  authDomain: "my-first-project-6c51a.firebaseapp.com",
-  projectId: "my-first-project-6c51a",
-  storageBucket: "my-first-project-6c51a.firebasestorage.app",
-  messagingSenderId: "233816787125",
-  appId: "1:233816787125:web:b7c15bcd4cb1a5b02956fc",
-  measurementId: "G-H16FB6Y2TC"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
